f91193fbc9e460d53cced1fb656b214b3d20cdd7
Logan Yang <logan1934@gmail.com>
2015-09-05 19:29:31 -0700

    Add hash33 and max tree

---
+++ dataStructure/hash33.py
@@ -0,0 +1,20 @@
+# hash33: http://www.lintcode.com/en/problem/hash-function/
+# python: ord("c"), return ascii
+# hash("abcd") = (a * 33^3 + b * 33^2 + c * 33 + d) % hashtable_size
+
+
+class Solution:
+    """
+    @param key: A String you should hash
+    @param HASH_SIZE: An integer
+    @return an integer
+    """
+    def hashCode(self, key, HASH_SIZE):
+        ascii_list = [ord(c) for c in key]
+        sum = 0
+        for ascii in ascii_list:
+            sum = sum * 33 + ascii
+            sum = sum % HASH_SIZE
+        return sum
+
+print ord("c")
--- dataStructure/maxtree.py
+++ dataStructure/maxtree.py
@@ -12,24 +12,37 @@ class Solution:
     # @param A: Given an integer array with no duplicates.
     # @return: The root of max tree.
     def maxTree(self, A):
+        # a decreasing stack, while new > old, pop old
+        # an item i's left is its 1st bigger to the left
+        # the item that kicks i out is i's 1st bigger to the right
         stack = []
         root = None
         for i in xrange(len(A)+1):
-            right = TreeNode(float("inf")) if i == len(A) else TreeNode(A[i])
+            # use inf to kick out all elements in stack
+            root = TreeNode(float("inf")) if i == len(A) else TreeNode(A[i])
 
             while len(stack) != 0:
-                if right.val > stack[-1].val:
+                # [.., left, popNode], current
+                # if current > popNode, current is popNode's 1st bigger to right
+                if root.val > stack[-1].val:
                     popNode = stack.pop()
+                    # stack empty, meaning it has no bigger to the left
+                    # popNode is the left child of current
                     if len(stack) == 0:
-                        right.left = popNode
+                        root.left = popNode
                     else:
                         left = stack[-1]
-                        if left.val > right.val:
-                            right.left = popNode
+                        # new top: left, if bigger than current, left > popNode < root
+                        # where left is biggest. left is popNode's first bigger to left
+                        # popNode is the smaller in the two's child
+                        # if left > root, popNode is root's left child since root is on its right
+                        if left.val > root.val:
+                            root.left = popNode
+                        # if root > left, popNode is left's right child
                         else:
                             left.right = popNode
                 else:
                     break
-            stack.append(right)
+            stack.append(root)
 
         return stack[-1].left
