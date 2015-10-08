c674002150494c77090bf3971c732ffc757fb34b
Logan Yang <logan1934@gmail.com>
2015-10-07 23:56:59 -0700

    Add word search WordPath

--- leetcode/wordSearch.py
+++ leetcode/wordSearch.py
@@ -1,14 +1,13 @@
 # wordSearch: https://leetcode.com/problems/word-search/
 # The question's input example is wrong. Input should be list of strings,
 # not list of list of strings.
-# TEMPLATE: RECURSION PATH SEARCH， TRUE OR FALSE
 
 
 class Solution(object):
 
     def exist(self, board, word):
         """
-        :type board: List[List[str]]
+        :type board: List[str]
         :type word: str
         :rtype: bool
         """
@@ -58,12 +57,49 @@ class Solution(object):
             board[x][y] = word[0]
         return False
 
+class WordPath:
+    """
+    @return: List[List[Tuples]], all possible paths that forms the word in the board
+    """
+    def find_path(self, board, word):
+        self.result = []
+        my_board = [list(string) for string in board]
+        for x in xrange(len(my_board)):
+            for y in xrange(len(my_board[0])):
+                path = []
+                if x == 0 and y == 2:
+                    self.dfs(my_board, word, x, y, path)
+        return self.result
+
+    def dfs(self, board, word, x, y, path):
+        # base: Success
+        if len(word) == 0:
+            # do not forget make new list()
+            self.result.append(list(path))
+            return True
+        # base: Fail
+        if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
+            return False
+        # recursion
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        if board[x][y] == word[0]:
+            for direction in directions:
+                board[x][y] = None
+                new_x, new_y = x + direction[0], y + direction[1]
+                path.append((x, y))
+                if self.dfs(board, word[1:], new_x, new_y, path):
+                    return True
+                path.pop()
+                board[x][y] = word[0]
+        return False
+
 board = [
   "ABCE",
   "SFCS",
   "ADEE"
 ]
 word = "CBF"
-Sol = Solution()
-
-print Sol.exist(board, word)
+# Sol = Solution()
+Path = WordPath()
+# print Sol.exist(board, word)
+print Path.find_path(board, word)
